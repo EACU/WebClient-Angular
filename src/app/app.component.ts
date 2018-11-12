a8431068797d581c6241957cc728ef9d1b552f4a
eqip3u <eqip3u.dem@yandex.ru>
2018-11-12 20:53:03 +0500

    sidenav responsive-mobile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { UserService } from 'src/shared/services/user.service';
 
 @Component({
@@ -7,12 +7,13 @@ import { UserService } from 'src/shared/services/user.service';
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
 
-  constructor(private userService: UserService) {
+  constructor(private userService: UserService) { }
+
+  ngOnInit(): void {
     if (!!localStorage.getItem('accessToken')) {
       this.userService.getApiUserInformation().subscribe(user => this.userService.setCurrentUser(user));
     }
   }
-
 }
--- src/app/main-nav/main-nav.component.ts
+++ src/app/main-nav/main-nav.component.ts
@@ -1,20 +1,32 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, ChangeDetectorRef, OnDestroy } from '@angular/core';
 
 import { UserService } from 'src/shared/services/user.service';
 import { SidenavService } from 'src/shared/services/sidenav.service';
 
 import { MatSidenav } from '@angular/material';
+import { MediaMatcher } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-main-nav',
   templateUrl: './main-nav.component.html',
   styleUrls: ['./main-nav.component.css']
 })
-export class MainNavComponent implements OnInit {
+export class MainNavComponent implements OnInit, OnDestroy {
 
   @ViewChild('sidenav') public sidenav: MatSidenav;
 
-  constructor(private userService: UserService, private sidenavService: SidenavService) { }
+  mobileQuery: MediaQueryList;
+  private _mobileQueryListener: () => void;
+
+  constructor(
+    changeDetectorRef: ChangeDetectorRef,
+    media: MediaMatcher,
+    private userService: UserService,
+    private sidenavService: SidenavService) {
+      this.mobileQuery = media.matchMedia('(max-width: 600px)');
+      this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+      this.mobileQuery.addListener(this._mobileQueryListener);
+  }
 
   ngOnInit(): void {
     this.sidenavService.setSidenav(this.sidenav);
@@ -28,5 +40,8 @@ export class MainNavComponent implements OnInit {
     return this.userService.isMatchRoleUser(role);
   }
 
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
 }
 
